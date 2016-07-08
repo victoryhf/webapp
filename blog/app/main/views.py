ce6e1397350149d29059996753d86761c81b7947
root <root@splunk.(none)>
2016-07-08 15:29:04 +0800

    user's profile 160708 by hf

--- blog/app/main/views.py
+++ blog/app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from ..models import User
 
 
 @main.route('/')
@@ -7,3 +8,9 @@ def index():
     return render_template('index.html')
 
 
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
--- blog/app/models.py
+++ blog/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
@@ -8,7 +9,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    default1 = db.Column(db.Boolean, default=False, index=True)
+    default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
    
@@ -55,6 +56,11 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -113,6 +119,11 @@ class User(UserMixin, db.Model):
         self.confirmed = True
         db.session.add(self)
         return True
+
+    def ping(self):
+	self.last_seen = datetime.utcnow()
+	db.session.add(self)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
